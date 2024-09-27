263d5803dded7de1f6ec6dbd46ba848e9f4e31c8
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-09-27 08:37:59 +0100

    chore: Update puppet manifest to install Flask version 2.1.0 using pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 #!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package {'flask':
+
+# Ensure Python3 is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask version 2.1.0 using pip3
+python::pip { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  pip_provider => 'pip3',
 }
