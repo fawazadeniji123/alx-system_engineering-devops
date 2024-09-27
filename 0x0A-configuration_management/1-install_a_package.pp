7f7a6fa36193cc673e8ef4e09bf4a884462abef2
Fawaz Adeniji <fawazadeniji123@gmail.com>
2024-09-27 08:47:40 +0100

    revert to original

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,7 @@
 #!/usr/bin/pup
 
-# Ensure Python3 is installed
-package { 'python3-pip':
-  ensure => installed,
-}
-
-# Install Flask version 2.1.0 using pip3
-python::pip { 'flask':
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  pip_provider => 'pip3',
+  provider => 'pip3'
 }
-
